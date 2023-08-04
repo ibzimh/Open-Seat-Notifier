13095d10d891e85301298cfda31e8e5d4ff322b9
ibzimh <ibzimh@gmail.com>
2023-08-04 16:31:14 +0500

    changing cookies

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import { readFromJSONFile, writeToJSONFile } from "./fileUtility.js";
 import { fetch_course_data } from "./fetch.js";
 
-readFromJSONFile("subjects.json").then(courses =>
-  Promise.all(courses.map(course => fetch_course_data(course.id))).then(
-    values => writeToJSONFile("data.json", values)
-  )
-);
+async function store_open_courses() {
+  return readFromJSONFile("subjects.json").then(courses =>
+    Promise.all(courses.map(course => fetch_course_data(course.id))).then(
+      values => writeToJSONFile("data.json", values)
+    )
+  );
+}
+
+store_open_courses();
